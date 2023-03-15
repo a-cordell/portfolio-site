ae13f91d2d612a1c75cc32dbbe21ae5166b3217e
a-cordell <acordell@hawk.iit.edu>
2023-03-15 11:48:36 -0500

    Add starter geometry
    Added torus knot as a test for animation loop

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
+//perspective camera mimics how the human eye sees things
+//arguments: FOV, aspect ratio(browser ratio), view frustrum(what is visible to the camera)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
@@ -11,6 +13,26 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
+//full screen canvas
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+//draws the scene through the camera perspective
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusKnotGeometry(10,3,16,100);
+//mesh standard: reacts to light
+//mesh basic: simply the vertices
+const material =  new THREE.MeshBasicMaterial({color:0xFF6347, wireframe: true});
+const torusKnot = new THREE.Mesh(geometry, material);
+
+scene.add(torusKnot);
+
+//continuous loop recursive function
+//updates scene constantly, re-renders
+function animate() {
+  //tells the browser you want to perform an animation
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
